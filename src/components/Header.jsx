c13b8052471f7324de8531767a450afb209ac495
ZackAMC <andresmila01@gmail.com>
2023-11-05 17:25:30 -0500

    nofue mucnho pero fue melo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
 
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes {''}
+            Seguimiento Pacientes {''} Con Github {''}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
