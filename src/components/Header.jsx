8f54f9d9cf5e6c4616842022606a1f36657de5eb
ZackAMC <andresmila01@gmail.com>
2024-01-06 10:23:30 -0500

    correccion texto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
 
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes {''} Con Github {''}
+            Seguimiento Pacientes {''}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
